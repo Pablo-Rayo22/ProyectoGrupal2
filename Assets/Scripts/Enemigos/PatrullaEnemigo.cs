25edc8ae9cb66e5c60f3a02172404da1a14d75bf
Pablo-Rayo22 <pablodky.8@gmail.com>
2026-05-15 20:37:52 +0200

    Pablo_Logica_Patrulla_Finalizada

--- Assets/Scripts/Enemigos/PatrullaEnemigo.cs
+++ Assets/Scripts/Enemigos/PatrullaEnemigo.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Rendering;
 
 public class PatrullaEnemigo : MonoBehaviour
 {
     private bool corutinaActiva = false;
     private Vector3 posicionInicial;
-    private Quaternion rotacionInicial;
-    private float segundos;
     public Transform objetivo;
     public Quaternion rotacionFinal;
     public float tiempoInterpolacion = 0f;
-    public float tiempoRotacion = 1f;
+    public float tiempoRotacion = 0f;
 
     private void Awake()
     {
@@ -22,9 +21,8 @@ public class PatrullaEnemigo : MonoBehaviour
         if (!corutinaActiva)
         {
             corutinaActiva = true;
-            //StartCoroutine(Rotar(rotacionInicial, rotacionFinal, tiempoInterpolacion));
-            //StartCoroutine(Patrullar(posicionInicial, objetivo, tiempoInterpolacion));
             StartCoroutine(Rotar());
+            //StartCoroutine(Rotar(rotacionInicial, rotacionFinal, tiempoInterpolacion));
         }
     }
 
@@ -32,14 +30,6 @@ public class PatrullaEnemigo : MonoBehaviour
     {
         StopAllCoroutines();
     }
-
-    //private IEnumerator SecuenciaPatrulla()
-    //{
-    //    yield return StartCoroutine(Rotar());
-    //    yield return StartCoroutine(Patrullar());
-    //    cambiarObjetivo();
-    //    corutinaActiva = false;
-    //}
 
     private IEnumerator Patrullar()
     {
@@ -52,28 +42,30 @@ public class PatrullaEnemigo : MonoBehaviour
         }
         objetivo.position = transform.position;
         cambiarObjetivo();
-        //corutinaActiva = false;
     }
     private IEnumerator Rotar()
     {
         Vector3 direccion = (objetivo.position - transform.position).normalized;
+
+        Quaternion rotacionInicial = transform.rotation;
         float rotacionY = cambiarRotacionY();
-
-        rotacionInicial = transform.rotation;
-
-        rotacionFinal = Quaternion.Euler(270f, rotacionY, 0f);
         float tiempoTranscurrido = 0f;
-        //segundos = 2f; 
         while (tiempoTranscurrido < tiempoRotacion)
         {
-            transform.rotation = Quaternion.Lerp(rotacionInicial, rotacionFinal, tiempoTranscurrido / tiempoRotacion);
+            transform.rotation = Quaternion.Lerp(rotacionInicial, rotacionFinal, tiempoTranscurrido / tiempoInterpolacion);
             tiempoTranscurrido += Time.deltaTime;
             yield return null; // Para detener la corrutina en el siguiente frame
         }
         transform.rotation = rotacionFinal;
         yield return StartCoroutine(Patrullar());
-        //cambiarRotacion();
         corutinaActiva = false;
+    }
+
+    private void cambiarObjetivo()
+    {
+        // 
+        objetivo.position = posicionInicial;
+        posicionInicial = transform.position;
     }
 
     private float cambiarRotacionY()
@@ -89,19 +81,4 @@ public class PatrullaEnemigo : MonoBehaviour
             return rotacionIzquierda;
         }
     }
-
-    private void cambiarObjetivo()
-    {
-        // 
-        objetivo.position = posicionInicial;
-        posicionInicial = transform.position;
-    }
-
-    //private void cambiarRotacion()
-    //{
-    //    //rotacionFinal = transform.rotation;
-
-    //    //rotacionFinal = rotacionInicial;
-    //    //rotacionInicial = transform.rotation;
-    //}
 }
